fdd91921dcf271fda78d2b116b03086b904236a3
devbn3li <fudwhc@gmail.com>
2024-03-05 01:23:32 +0200

    fix

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,4 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employeeNames = [...reportWithIterator].join('|');
-  return employeeNames;
+  let result = '';
+  for (const name of reportWithIterator) {
+    if (result) {
+      result += ` | ${name}`;
+    } else {
+      result = name;
+    }
+  }
+  return result;
 }
