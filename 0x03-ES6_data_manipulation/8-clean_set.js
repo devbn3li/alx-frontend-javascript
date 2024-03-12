10564741ec983d9ffacf62a84c01e6579dea5862
devbn3li <fudwhc@gmail.com>
2024-03-12 23:19:28 +0200

    8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+function cleanSet(set, startString) {
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  return filteredValues
+    .map((value) => value.substring(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
