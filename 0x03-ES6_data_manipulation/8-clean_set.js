2116c99b48a2f24ea090c6bdd64d9228f005f976
devbn3li <fudwhc@gmail.com>
2024-03-12 23:25:41 +0200

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,20 @@
-function cleanSet(set, startString) {
-  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
-  return filteredValues
-    .map((value) => value.substring(startString.length))
-    .join('-');
-}
+export default function cleanSet(set, startString) {
+  let str = '';
 
-export default cleanSet;
+  if (startString === '') {
+    return '';
+  }
+  for (const item of set) {
+    if (set instanceof Set && typeof startString === 'string') {
+      if (item && item.startsWith(startString)) {
+        const strSlice = item.slice(startString.length);
+        if (str !== '') {
+          str += '-';
+        }
+        str += strSlice;
+      }
+    }
+  }
+
+  return str;
+}
